71a540215b3e1da6c99e814773874374d712520b
Daniel Kostro <kostro.d@gmail.com>
2017-05-16 10:05:09 +0200

    Move main content to separate component

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,7 @@ import NodeListDrawer from './NodeListDrawer';
 
 import AppBar from 'material-ui/AppBar';
 
-import Block from './Block';
-import SvgLines from './SvgLines';
-
-const testDefinition = {
-    name: 'testnode',
-    label: 'My test node',
-    inputs: [
-        {name: 'input1'},
-        {name: 'input2', multiple: true},
-        {name: 'input3'}
-    ],
-    outputs: [
-        {name: 'output1'},
-        {name: 'output2'}
-    ],
-    options: null
-};
-
-const testDefinition2 = {
-    name: 'test2',
-    inputs: [
-        {name: 'input'}
-    ]
-};
+import MainContent from './MainContent';
 
 export default function () {
     return (
@@ -36,13 +13,7 @@ export default function () {
             <div style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
                 <AppBar title="larissa" />
                 <NodeListDrawer />
-                <div style={{flex: 0.99}}>
-                    <div style={{position: 'relative', height: '100%'}}>
-                        <SvgLines lines={[[[612, 202], [735, 227]]]} />
-                        <Block status="error" definition={testDefinition} style={{top: 150, left: 350}} />
-                        <Block status="running" definition={testDefinition2} style={{position: 'absolute', top: 150, left: 750}} />
-                    </div>
-                </div>
+                <MainContent />
             </div>
         </MuiThemeProvider>
     );
