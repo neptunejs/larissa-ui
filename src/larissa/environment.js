cbe490aee150d18361fb00f63c9dd04a18ec5c5a
Michaël Zasso <targos@protonmail.com>
2017-06-02 14:52:25 +0200

    add logic larissa plugin

--- src/larissa/environment.js
+++ src/larissa/environment.js
@@ -1,7 +1,9 @@
 import {Environment} from 'larissa';
 import imagePlugin from 'larissa-plugin-nodes-image-js';
+import logicPlugin from 'larissa-plugin-nodes-logic';
 
 const env = new Environment();
 env.loadPlugin(imagePlugin());
+env.loadPlugin(logicPlugin());
 
 export default env;
