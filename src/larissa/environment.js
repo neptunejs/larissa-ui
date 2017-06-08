b9b293444908b87d603e30a2ca204655f877910f
Michaël Zasso <targos@protonmail.com>
2017-06-08 15:49:06 +0200

    add mljs plugin

--- src/larissa/environment.js
+++ src/larissa/environment.js
@@ -1,9 +1,11 @@
 import {Environment} from 'larissa';
 import imagePlugin from 'larissa-plugin-nodes-image-js';
 import logicPlugin from 'larissa-plugin-nodes-logic';
+import mljsPlugin from 'larissa-plugin-nodes-mljs';
 
 const env = new Environment();
 env.loadPlugin(imagePlugin());
 env.loadPlugin(logicPlugin());
+env.loadPlugin(mljsPlugin());
 
 export default env;
