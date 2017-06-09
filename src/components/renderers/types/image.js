26a1dc4cb81307143af9c703fbfe291d96bcaa79
Michaël Zasso <targos@protonmail.com>
2017-06-09 10:58:00 +0200

    better image preview

--- src/components/renderers/types/image.js
+++ src/components/renderers/types/image.js
@@ -1,12 +1,32 @@
-import {createElement} from 'react';
+import {createElement, Component} from 'react';
 
-export function image({value}) {
-    return (
-        <div style={{
-            backgroundImage: `url(${value.toDataURL()})`,
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-            height: 30
-        }} />
-    );
+export class image extends Component {
+    constructor(props) {
+        super(props);
+        this.value = props.value.toDataURL();
+        this.smallValue = props.value.scale({height: 50}).toDataURL();
+        this.state = {
+            hovering: false
+        };
+    }
+
+    render() {
+        return (
+            <div
+                onMouseEnter={() => this.setState({hovering: true})}
+                onMouseLeave={() => this.setState({hovering: false})}
+            >
+                {
+                    this.state.hovering ?
+                        <img src={this.value} /> :
+                        <div style={{
+                            backgroundImage: `url(${this.smallValue})`,
+                            backgroundRepeat: 'no-repeat',
+                            width: '100%',
+                            height: 50
+                        }} />
+                }
+            </div>
+        )
+    }
 }
