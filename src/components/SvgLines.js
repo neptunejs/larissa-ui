ee00205d992f03e9b4b4ac319fb8f4b5fb6abffd
Michaël Zasso <targos@protonmail.com>
2017-06-15 14:20:13 +0200

    add a transition to SVG lines

--- src/components/SvgLines.js
+++ src/components/SvgLines.js
@@ -6,7 +6,7 @@ export default function SvgLines({lines = []}) {
         const [sP, eP] = lines[i];
         const hD = (eP[0] - sP[0]) / 3;
         inlines.push(
-            <path key={i} d={`M${sP[0]},${sP[1]} C${eP[0] - hD},${sP[1]} ${sP[0] + hD},${eP[1]} ${eP[0]},${eP[1]}`} stroke="black" fill="none" />
+            <path key={i} d={`M${sP[0]},${sP[1]} C${eP[0] - hD},${sP[1]} ${sP[0] + hD},${eP[1]} ${eP[0]},${eP[1]}`} stroke="black" fill="none" style={{transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'}} />
         );
     }
     return (
