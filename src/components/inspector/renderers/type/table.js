fc42c29453f9a2f5192526010c5358ea805f8109
Daniel Kostro <kostro.d@gmail.com>
2017-06-19 13:53:43 +0200

    better table renderer

--- src/components/inspector/renderers/type/table.js
+++ src/components/inspector/renderers/type/table.js
@@ -1,27 +1,56 @@
 import {createElement, Component} from 'react';
 import {ScatterPlot} from 'react-d3-components';
 import {inspectorWidth} from '../../../../constants';
-import {SelectField} from 'material-ui';
+import {SelectField, MenuItem} from 'material-ui';
 var tooltipScatter = function (x, y) {
     return 'x: ' + x + ' y: ' + y;
 };
+import {groupBy} from 'lodash-es';
 
 class TableRenderer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            color: null;
-        }
+            xIdx: 0,
+            yIdx: 1,
+            colorIdx: -1
+        };
     }
+
+    onColorChange(event, idx) {
+        this.setState({
+            colorIdx: idx - 1
+        });
+    }
+
+    onXChange(event, idx) {
+        this.setState({
+            xIdx: idx
+        });
+    }
+
+    onYChange(event, idx) {
+        this.setState({
+            yIdx: idx
+        });
+    }
+
     render() {
         const data = this.props.data;
 
-        let chartData = [
-            {
-                label: 'A',
-                values: data.data.map(d => ({x: d[0], y: d[1]}))
-            }
-        ];
+        const xAccessor = (row) => row[this.state.xIdx];
+        const yAccessor = (row) => row[this.state.yIdx];
+        let chartData = [];
+        let grouped = groupBy(data.data, row => {
+            return row[this.state.colorIdx];
+        });
+
+        for (let key in grouped) {
+            chartData.push({
+                label: key,
+                values: grouped[key]
+            });
+        }
 
         // compute axis zeros
         // const xMin = Math.min(...(data.data.map(d => d[0])));
@@ -29,21 +58,43 @@ class TableRenderer extends Component {
 
         // console.log(xMin, yMin);
 
-        <SelectField style={{width: '100%'}} floatingLabelText="Select" value={this.state.color}
-                     onChange={this.onColorChange.bind(this)}>
-            {this.props.candidates.map(renderCandidate)}
-        </SelectField>
 
         return (
             <div>
-                <SelectField />
+                <SelectField
+                    // style={{width: '100%'}}
+                    floatingLabelText="Select X"
+                    value={this.state.xIdx}
+                    onChange={this.onXChange.bind(this)}
+                >
+                    {data.headers.map(renderLabel)}
+                </SelectField>
+                <SelectField
+                    // style={{width: '100%'}}
+                    floatingLabelText="Select Y"
+                    value={this.state.yIdx}
+                    onChange={this.onYChange.bind(this)}
+                >
+                    {data.headers.map(renderLabel)}
+                </SelectField>
+                <SelectField
+                    style={{width: '100%'}}
+                    floatingLabelText="Select color"
+                    value={this.state.colorIdx}
+                    onChange={this.onColorChange.bind(this)}
+                >
+                    <MenuItem value={-1} key={-1} primaryText="None" />
+                    {data.headers.map(renderLabel)}
+                </SelectField>
                 <ScatterPlot
                     data={chartData}
-                    width={inspectorWidth}
-                    height={inspectorWidth}
+                    x={xAccessor}
+                    y={yAccessor}
+                    width={inspectorWidth - 20}
+                    height={inspectorWidth * 2 / 3}
                     tooltipHtml={tooltipScatter}
-                    xAxis={{zero: 0, label: data.headers[0]}}
-                    yAxis={{zero: 0, label: data.headers[1]}}
+                    xAxis={{zero: 0, label: data.headers[this.state.xIdx]}}
+                    yAxis={{zero: 0, label: data.headers[this.state.yIdx]}}
                 />
             </div>
         );
@@ -51,11 +102,12 @@ class TableRenderer extends Component {
     }
 }
 
-function renderCandidate(candidate) {
+function renderLabel(label, idx) {
     return (
-        <MenuItem value={candidate.info.id} key={candidate.info.id} primaryText={`${candidate.node.title} - ${candidate.info.name}`} />
+        <MenuItem
+            value={idx} key={idx} primaryText={label}
+        />
     );
 }
-
 
 export default TableRenderer;
