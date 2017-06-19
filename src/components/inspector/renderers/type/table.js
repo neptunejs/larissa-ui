2995678d1d9bba972d24aa05e03527ac4219687d
Daniel Kostro <kostro.d@gmail.com>
2017-06-19 12:48:51 +0200

    Merge branch 'master' of /home/stropitek/projects/neptunejs/larissa-ui with conflicts.

--- src/components/inspector/renderers/type/table.js
+++ src/components/inspector/renderers/type/table.js
@@ -1,8 +1,18 @@
 import {createElement, Component} from 'react';
 import {ScatterPlot} from 'react-d3-components';
 import {inspectorWidth} from '../../../../constants';
+import {SelectField} from 'material-ui';
+var tooltipScatter = function (x, y) {
+    return 'x: ' + x + ' y: ' + y;
+};
 
 class TableRenderer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: null;
+        }
+    }
     render() {
         const data = this.props.data;
 
@@ -13,18 +23,39 @@ class TableRenderer extends Component {
             }
         ];
 
+        // compute axis zeros
+        // const xMin = Math.min(...(data.data.map(d => d[0])));
+        // const yMin = Math.min(...(data.data.map(d => d[1])));
+
+        // console.log(xMin, yMin);
+
+        <SelectField style={{width: '100%'}} floatingLabelText="Select" value={this.state.color}
+                     onChange={this.onColorChange.bind(this)}>
+            {this.props.candidates.map(renderCandidate)}
+        </SelectField>
 
         return (
-            <ScatterPlot
-                data={chartData}
-                width={inspectorWidth}
-                height={inspectorWidth}
-                xAxis={{innerTickSize: 10, label: data.headers[0]}}
-                yAxis={{innerTickSize: 10, label: data.headers[1]}}
-            />
+            <div>
+                <SelectField />
+                <ScatterPlot
+                    data={chartData}
+                    width={inspectorWidth}
+                    height={inspectorWidth}
+                    tooltipHtml={tooltipScatter}
+                    xAxis={{zero: 0, label: data.headers[0]}}
+                    yAxis={{zero: 0, label: data.headers[1]}}
+                />
+            </div>
         );
 
     }
 }
+
+function renderCandidate(candidate) {
+    return (
+        <MenuItem value={candidate.info.id} key={candidate.info.id} primaryText={`${candidate.node.title} - ${candidate.info.name}`} />
+    );
+}
+
 
 export default TableRenderer;
