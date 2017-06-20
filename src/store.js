1655c7ec23acaf6db6472664a30d3e57e0937efd
Michaël Zasso <targos@protonmail.com>
2017-06-20 13:58:47 +0200

    persist drawer state
    Closes #3

--- src/store.js
+++ src/store.js
@@ -2,6 +2,9 @@ import {routerMiddleware} from 'react-router-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunkMiddleware from 'redux-thunk';
+import {createTransform, persistStore, autoRehydrate} from 'redux-persist';
+import {Map} from 'immutable';
+
 import history from './history';
 import env from './larissa/environment';
 import './index.css';
@@ -19,14 +22,31 @@ const middlewares = [
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(
-    applyMiddleware(...middlewares)
+    applyMiddleware(...middlewares),
+    autoRehydrate()
 );
 
 const store = createStore(reducers, enhancer);
-export default store;
+
+const drawerTransform = createTransform(
+    (state) => state,
+    (state) => {
+        return {
+            ...state,
+            selectedInspectorTabs: new Map(state.selectedInspectorTabs)
+        };
+    },
+    {whitelist: ['drawer']}
+);
+persistStore(store, {
+    transforms: [drawerTransform],
+    whitelist: ['drawer']
+});
 
 if (module.hot) {
     module.hot.accept('./reducers/index', () => {
         store.replaceReducer(reducers);
     });
 }
+
+export default store;
