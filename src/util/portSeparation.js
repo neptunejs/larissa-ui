2ad96e15f31e1e478d8e41c2d85ee5b60f617ac8
Michaël Zasso <targos@protonmail.com>
2017-07-06 14:45:21 +0200

    cache port separation until 10 ports

--- src/util/portSeparation.js
+++ src/util/portSeparation.js
@@ -1,6 +1,11 @@
 import evenSeparation from './evenSeparation';
 import {portSize, blockHeight} from '../constants';
 
+const cache = new Map();
+for (let i = 0; i < 10; i++) {
+    cache.set(i, portSeparation(i));
+}
+
 export default function portSeparation(n) {
-    return evenSeparation(n, portSize, blockHeight);
+    return cache.get(n) || evenSeparation(n, portSize, blockHeight);
 }
